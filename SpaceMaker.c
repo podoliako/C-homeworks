442ebbb55ee0f550b44ebc99405240e661b6faa8
podoliako <podoliako.ea@phystech.edu>
2021-09-18 01:02:36 +0300

    Done

--- SpaceMaker.c
+++ SpaceMaker.c
@@ -7,38 +7,41 @@ struct ich {
     char* words;
 };
 
-struct ich SpaceMaker(char* line, char* spaces){
+void split(char* string, char* delimiters, char** tokens, int* tokensCount){
     struct ich result;
-    result.words = malloc(sizeof(char) * strlen(line) * 2);
+    result.words = malloc(sizeof(char) * strlen(string) * 2);
     result.amount = 0;
-    for(int i = 0; i < strlen(line); i++){
+    for(int i = 0; i < strlen(string); i++){
         int is_space_found = 0;
-        for(int j = 0; j < strlen(spaces); j++){
-            if (line[i] == spaces[j]){
+        for(int j = 0; j < strlen(delimiters); j++){
+            if (string[i] == delimiters[j]){
                 result.amount += 1;
-                strcat(result.words, " space ");
+                strcat(result.words, "', '");
+
                 is_space_found = 1;
                 break;
             }
         }
-      //  printf("%s, %d spaces\n", result.words, result.amount);
-      //  printf("%c = ", line[i]);
-        if (is_space_found == 0) result.words[strlen(result.words)] = line[i];
-      //  printf("%c\n", result.words[strlen(result.words) - 1]);
+        if (is_space_found == 0) result.words[strlen(result.words)] = string[i];
     }
-    //printf("%s, %d spaces", result.words, result.amount);
-    return result;
+    *tokens = malloc(sizeof(char) * strlen(result.words) * 2);
+    strcat(*tokens, result.words);
+    //printf("%s\n", string);
+    *tokensCount = result.amount;
 }
 
 int main() {
     char* line;
     char* spaces;
+    int counter;
+    char* tokens;
     line = "gasdg gadgogoxn oadsg sgo osgtqw etao g ogo";
     printf("%s\n", line);
     spaces = " ";
-    struct ich exit;
-    exit = SpaceMaker(line, spaces);
-    printf("%s, %d spaces", exit.words, exit.amount);
+
+    split(line, spaces, &tokens, &counter);
+
+    printf("'%s', %d spaces", tokens, counter);
 
     return 0;
 }
