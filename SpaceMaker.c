7feb434e2c4e161d0396bca319c0bca51cf19aa3
podoliako <podoliako.ea@phystech.edu>
2021-09-28 14:21:10 +0300

    DONE

--- SpaceMaker.c
+++ SpaceMaker.c
@@ -2,46 +2,46 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct ich {
-    int amount;
-    char* words;
-};
 
 void split(char* string, char* delimiters, char** tokens, int* tokensCount){
-    struct ich result;
-    result.words = malloc(sizeof(char) * strlen(string) * 2);
-    result.amount = 0;
+    *tokensCount = 0;
+    tokens[0] = malloc(sizeof(char) * strlen(string));
+    int letters_in_string = 0;
     for(int i = 0; i < strlen(string); i++){
         int is_space_found = 0;
         for(int j = 0; j < strlen(delimiters); j++){
             if (string[i] == delimiters[j]){
-                result.amount += 1;
-                strcat(result.words, "', '");
-
+                *tokensCount += 1;
                 is_space_found = 1;
+                letters_in_string = 0;
+                tokens[*tokensCount] =  malloc(sizeof(char)*strlen(string));
                 break;
+
             }
         }
-        if (is_space_found == 0) result.words[strlen(result.words)] = string[i];
+        if (is_space_found == 0) {
+            tokens[*tokensCount][letters_in_string++] = string[i];
+        }
     }
-    *tokens = malloc(sizeof(char) * strlen(result.words) * 2);
-    strcat(*tokens, result.words);
-    //printf("%s\n", string);
-    *tokensCount = result.amount;
+
 }
 
 int main() {
     char* line;
     char* spaces;
     int counter;
-    char* tokens;
+    char** tokens;
     line = "gasdg gadgogoxn oadsg sgo osgtqw etao g ogo";
     printf("%s\n", line);
     spaces = " ";
 
-    split(line, spaces, &tokens, &counter);
+    split(line, spaces, tokens, &counter);
 
-    printf("'%s', %d spaces", tokens, counter);
+    for(int i = 0; i < counter; i++){
+            printf("%s, ", tokens[i]);
+    }
+
+    for(int i = 0; i < counter; i++) free(tokens[i]);
 
     return 0;
 }
