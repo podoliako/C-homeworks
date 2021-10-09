690e6825b6746fb322cb088c577b85acbf5f6aac
podoliako <podoliako.ea@phystech.edu>
2021-10-09 09:48:31 +0300

    It works

--- Process/main.c
+++ Process/main.c
@@ -1,22 +1,48 @@
-#include <sys/wait.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main() {
+    int client;
+    scanf("%d", &client);
 
-int N = 7;
-for (int i = 0; i < N; i++){
+    int max_length = 80;
+
+    char *file_1 ="fifo1";
+    if (mkfifo(file_1, 0666) < 0) printf("ERROR\n");
+
+    char * file_2 = "fifo2";
+    if (mkfifo(file_2, 0666) < 0) printf("ERROR\n");
+
     size_t pid = fork ();
 
-    if (pid == 0){
-        printf ("%d: %d -> %d\n", i + 1,  getppid(), getpid());
+    if(pid == 0){
+        char arr[max_length];
+        int is_running = 1;
+        int fd;
+        if(client == 1) fd = open(file_1, O_WRONLY);
+        else fd = open(file_2, O_WRONLY);
+        while (is_running){
+            fgets(arr, max_length, stdin);
+            write(fd, arr, strlen(arr)+1);
+
+        }
+        close(fd);
     }
 
-    else if (pid > 0){
-        wait(0);
-        printf ("%zu : kid is over\n", pid);
-        exit(i);
+    else if(pid > 0){
+        int fd1;
+        char str[max_length];
+        int is_running = 1;
+        if(client == 1) fd1 = open(file_2,O_RDONLY);
+        else fd1 = open(file_1, O_RDONLY);
+        while(is_running){
+            read(fd1, str, max_length);
+            printf("User%d: %s\n", client, str);
         }
+        close(fd1);
     }
 }
